31c098daeeb554cbffbfdfc294e3730b439251f4
fatcat22 <pandayt@163.com>
2018-07-20 11:42:34 +0800

    add openssl library. although it seems like not be used.

--- build.sh
+++ build.sh
@@ -21,6 +21,10 @@ if [ ! -d ./boost ];then
 	mkdir boost
 fi
 
+if [ ! -d ./openssl ];then
+	mkdir openssl
+fi
+
 ###install boost
 cd $LIBDOWNLOAD
 
@@ -41,7 +45,31 @@ if [ ! -d ./boost_1_66_0 ];then
 fi
 
 cd boost_1_66_0
-GCCJAM=`find ./ -name gcc.jam`
-sed -i "s/if \$(link) = shared/if \$(link) = shared \|\| \$(link) = static/g" $GCCJAM
+GCCJAM=`find . -name gcc.jam`
+sed -i -e "s/if \$(link) = shared/if \$(link) = shared \|\| \$(link) = static/g" $GCCJAM
 ./bootstrap.sh
 ./b2 --prefix=$COIN_DEPS/boost --build-dir=boost.build link=static runtime-link=static variant=release install
+
+
+###intstall openssl
+cd $LIBDOWNLOAD
+
+if [ ! -f ./openssl-1.0.2k.tar.gz ];then
+	wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
+	if [ ! -f ./openssl-1.0.2k.tar.gz ];then
+		echo "Error cannot download openssl-1.0.2k.tar.gz"
+		exit 1
+	fi
+fi
+
+if [ ! -d ./openssl-1.0.2k ];then
+	tar xf openssl-1.0.2k.tar.gz
+	if [ ! -d ./openssl-1.0.2k ];then
+		echo "Error cannot tar xf openssl-1.0.2k.tar.gz"
+		exit 1
+	fi
+fi
+
+cd openssl-1.0.2k
+./config --prefix=$COIN_DEPS/openssl no-shared enable-ec enable-ecdh enable-ecdsa -fPIC
+make && make install
