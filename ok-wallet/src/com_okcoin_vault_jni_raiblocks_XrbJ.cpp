52033e07944dd6f9e343b1b799fc3464f0395e24
fatcat22 <pandayt@163.com>
2018-10-23 21:20:15 +0800

    prevent concurrency

--- ok-wallet/src/com_okcoin_vault_jni_raiblocks_XrbJ.cpp
+++ ok-wallet/src/com_okcoin_vault_jni_raiblocks_XrbJ.cpp
@@ -3,6 +3,18 @@
 #include "test_helper.h"
 #include <regex>
 #include <assert.h>
+#include <mutex>
+
+std::mutex mutex;
+
+class AutoUnlock
+{
+public:
+  ~AutoUnlock()
+  {
+    mutex.unlock();
+  }
+};
 
 static jstring char2jstring(JNIEnv* env, const char* pat)
 {
@@ -29,11 +41,15 @@ static std::string jstring2stdstring(JNIEnv* env, const jstring& jstr)
   return res;
 }
 
-static jobjectArray string2jobjectArray(JNIEnv* env, const std::string& s)
+static jobjectArray strings2jobjectArray(JNIEnv* env, const std::vector<std::string>& s)
 {
-  jobjectArray result = env->NewObjectArray(1, env->FindClass("java/lang/String"), 0);
+  jobjectArray result = env->NewObjectArray(s.size(), env->FindClass("java/lang/String"), 0);
 
-  env->SetObjectArrayElement(result, 0, char2jstring(env, s.c_str()));
+  for (int i = 0; i < s.size(); i++)
+  {
+    env->SetObjectArrayElement(result, i, char2jstring(env, s[i].c_str()));
+  }
+
   return result;
 }
 
@@ -58,6 +74,11 @@ std::vector<std::string> split_by_regex(const std::string& s, const char* patter
 JNIEXPORT jobjectArray JNICALL
 Java_com_okcoin_vault_jni_raiblocks_XrbJ_execute(JNIEnv *env, jclass, jstring networkType, jstring _command)
 {
+  if (mutex.try_lock() == false) {
+    return strings2jobjectArray(env, {"Error", "JNI_LOCKED"});
+  }
+  AutoUnlock au;
+
   static struct {
     const char* cmd_name;
     std::function<jobjectArray(const std::vector<std::string>& args)> handler;
@@ -65,46 +86,61 @@ Java_com_okcoin_vault_jni_raiblocks_XrbJ_execute(JNIEnv *env, jclass, jstring ne
     {
       "getaddressbyprivatekey", [env](const std::vector<std::string>& args)->jobjectArray{
         assert(1 == args.size());
+        std::string result("SUCCESS");
         std::string address;
         if (true != GetAddressFromPrivateKey(args[0], address)) {
           address.clear();
+          result = "Error";
         }
 
-        return string2jobjectArray(env, address);
+        return strings2jobjectArray(env, {result, address});
       }
     },
     {
       "createrawtransaction", [env, &networkType](const std::vector<std::string>& args)->jobjectArray {
         assert(4 == args.size());
 
+        std::string result("SUCCESS");
         std::string utx;
         const std::string& net_type = jstring2stdstring(env, networkType);
         if (true != produceUnsignedTx(args[0], args[1], args[2], args[3], utx)) {
           utx.clear();
+          result = "Error";
         }
 
-        return string2jobjectArray(env, utx);
+        return strings2jobjectArray(env, {result, utx});
       }
     },
     {
       "signrawtransaction", [env, &networkType](const std::vector<std::string>& args)->jobjectArray {
         assert(2 == args.size());
 
+        std::string result("SUCCESS");
         std::string stx;
         const std::string& net_type = jstring2stdstring(env, networkType);
         if (true != signTransaction(args[0], args[1], net_type, stx)) {
           stx.clear();
+          result = "Error";
         }
 
-        return string2jobjectArray(env, stx);
+        return strings2jobjectArray(env, {result, stx});
       }
     }
   };
 
+  try
+  {
+
+  }
+  catch (const std::exception& e)
+  {
+      printf("execute except: %s\n", e.what());
+      return 0;
+  }
   const auto& command = jstring2stdstring(env, _command);
   const auto& cmd_vec = split_by_regex(command, "(\\S+)");
   if (cmd_vec.empty()) {
-    return jobjectArray();
+    return strings2jobjectArray(env, {"Error", "Invalid command"});
   }
 
   for (auto& h : command_handlers) {
@@ -115,5 +151,5 @@ Java_com_okcoin_vault_jni_raiblocks_XrbJ_execute(JNIEnv *env, jclass, jstring ne
     return h.handler(std::vector<std::string>(cmd_vec.begin() + 1, cmd_vec.end()));
   }
 
-  return jobjectArray();
+  return strings2jobjectArray(env, {"Error", "Unknown command"});
 }
